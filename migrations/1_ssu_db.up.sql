ee8b1032891fc4cbc6eb98b20084fce731b9ac1b
PosokhovVadim <posokhovvadyusha@gmail.com>
2023-04-20 11:05:06 +0400

    added table(groups)

--- migrations/1_ssu_db.up.sql
+++ migrations/1_ssu_db.up.sql
@@ -2,3 +2,10 @@ CREATE TABLE departments (
     url varchar not null,
     name varchar not null
 );
+
+CREATE TABLE groups (
+    dep varchar not null,
+    ed-form varchar not null,
+    groupNum varchar not null,
+    CONSTRAINT constraint_groups PRIMARY KEY(dep, edd-form)
+)
